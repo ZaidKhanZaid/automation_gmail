997f212d7d631483d0c1b8b28112a5322a03369a
ZaidKhanZaid <zaidkhan.29.2002@gmail.com>
2025-12-27 15:26:58 +0530

    final commit

---
+++ src/com/gmail/CommonService.java
@@ -0,0 +1,15 @@
+package com.gmail;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class CommonService {
+
+	public static WebDriver getChromeDriver() {
+		System.setProperty(CommonConstant.WEB_CHROME_DRIVER, CommonConstant.WEB_CHROME_DRIVER_PATH);
+		WebDriver driver = new ChromeDriver();
+		return driver;
+	}
+}
